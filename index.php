cd15718187ad98383bcf8e35a9b67a0d5db670b6
Manav2626 <manavwork01@gmail.com>
2024-08-12 17:09:21 +0530

    mail setup

--- index.php
+++ index.php
@@ -316,7 +316,7 @@
 									<div>
 										<!--Portfolio item-->
 										<article class="vlt-work vlt-work--style-3">
-											<div class="vlt-work__media"><a class="vlt-work__link has-cursor" href="./allservices.php"></a><img src="assets/img1/ms1.jpg" alt="" loading="lazy">
+											<div class="vlt-work__media"><img src="assets/img1/ms1.jpg" alt="" loading="lazy">
 											</div>
 											
 										</article>
@@ -326,7 +326,7 @@
 									<div>
 										<!--Portfolio item-->
 										<article class="vlt-work vlt-work--style-3">
-											<div class="vlt-work__media"><a class="vlt-work__link has-cursor" href="./allservices.php"></a><img src="assets/img1/ms2.jpg" alt="" loading="lazy">
+											<div class="vlt-work__media"><img src="assets/img1/ms2.jpg" alt="" loading="lazy">
 											</div>
 											
 										</article>
@@ -336,7 +336,7 @@
 									<div>
 										<!--Portfolio item-->
 										<article class="vlt-work vlt-work--style-3">
-											<div class="vlt-work__media"><a class="vlt-work__link has-cursor" href="./allservices.php"></a><img src="assets/img1/ms4.jpg" alt="" loading="lazy">
+											<div class="vlt-work__media"><img src="assets/img1/ms4.jpg" alt="" loading="lazy">
 											</div>
 											
 										</article>
@@ -346,7 +346,7 @@
 									<div>
 										<!--Portfolio item-->
 										<article class="vlt-work vlt-work--style-3">
-											<div class="vlt-work__media"><a class="vlt-work__link has-cursor" href="./allservices.php"></a><img src="assets/img1/ms5.jpg" alt="" loading="lazy">
+											<div class="vlt-work__media"><img src="assets/img1/ms5.jpg" alt="" loading="lazy">
 											</div>
 											
 										</article>
